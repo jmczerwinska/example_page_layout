e1a41c656aae213d7b9a8ab844a0b62afbf94d75
jmczerwinska <asia.frej@gmail.com>
2020-03-07 21:01:40 +0100

    add event listner to window

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const hamburgerMenu =document.getElementById('hamburger-menu');
 hamburgerBtn.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('hamburger-menu--show');
 });
+
+window.addEventListener('click', (e) =>  {
+    console.log("########")
+    console.log(e.target.classList)
+    const a = hamburgerMenu.classList.contains('hamburger-menu--show')
+    const b = (e.target !== hamburgerMenu)
+    const c = (e.target !== hamburgerBtn)
+    console.log(`==> ${a} ${b} ${c}`)
+
+    if ( e.target !== hamburgerMenu 
+        && e.target !== hamburgerBtn 
+        && e.target.parentNode !== hamburgerBtn) {
+        hamburgerMenu.classList.remove('hamburger-menu--show');
+    }
+    
+})
